ac295d73ab133b878b1a57c473c5f35c66f42358
@ajessondanie <aurelien.jesson-daniel@etu.u-bordeaux.fr>
2025-04-02 01:10:20 +0200

    le json des enfers

--- back/assets/trieJson.js
+++ back/assets/trieJson.js
@@ -1,181 +1,330 @@
-const fs = require('fs');
+const fs = require("fs");
+const axios = require("axios");
+const listeExtensions = require("./listeExtensions.json");
+// -------------- TRIE DU JSON  ----------------
 
-// -------------- TRIE DU JSON  ----------------    
+// function qui supprime cleaned_test.json et jsonFR et compare.json si ils existent
+function deleteFiles() {
+  const filesToDelete = ["cleaned_test.json", "jsonFR.json", "compare.json"];
+  filesToDelete.forEach((file) => {
+    if (fs.existsSync(file)) {
+      fs.unlinkSync(file);
+      console.log(`Deleted ${file}`);
+    } else {
+      console.log(`${file} does not exist`);
+    }
+  });
+}
 
+// TODO LE VEILLE AMBRE A SET_NAME NULL PENSER A LE REMETTRE
 
+//  ETAPE 1 : QUAND NOUVELLE EXTESNION, FAIRE CE CODE
+async function ETAPE1() {
+  try {
+    const response = await axios.get(
+      "https://api.dotgg.gg/cgfw/getcards?game=pokepocket&mode=indexed&cache=192"
+    );
+    const jsonData = response.data;
 
-//  Read the JSON file
-// fs.readFile('cards.json', 'utf8', (err, data) => {
+    const cleanedData = jsonData.data.map((card) => {
+      const dupeReward = Array.isArray(card[23])
+        ? card[23].find((prop) => prop.name === "Dupe Reward")?.value ?? null
+        : null;
+
+      const packPoint = Array.isArray(card[23])
+        ? card[23].find((prop) => prop.name === "Pack Point")?.value ?? null
+        : null;
+
+      // transforme les strings comme 1,200 en int 1200
+      function parsePackPoint(value) {
+        if (typeof value === "string") {
+          // Supprime les virgules et convertit en entier
+          return parseInt(value.replace(/,/g, ""), 10);
+        }
+        return value;
+      }
+
+      // Détermine la rareté en fonction de packPoint
+      const rarityMap = {
+        35: 1,
+        70: 2,
+        150: 3,
+        500: 4,
+        400: 5,
+        1250: 6,
+        1500: 7,
+        1000: 8,
+        1350: 9,
+        2500: 10,
+      };
+
+      const rarity = rarityMap[parsePackPoint(packPoint)] ?? null; // Définit null si packPoint n'est pas dans la liste
+
+      // Détermine le coût d'échange en fonction de la rareté
+      const tradeCostMap = {
+        1: 0,
+        2: 0,
+        3: 120,
+        4: 500,
+        5: 400,
+        6: 0,
+        7: 0,
+        8: 0,
+        9: 0,
+        10: 0,
+        null: 0,
+      };
+
+      const tradeCost = tradeCostMap[rarity] ?? null; // Définit null si la rareté n'est pas dans la liste
+
+      return {
+        id: card[0],
+        setId: card[1],
+        dupe_reward: parsePackPoint(dupeReward),
+        pack_point: parsePackPoint(packPoint),
+        rarity: rarity,
+        trade_cost: tradeCost,
+        isTradable: null,
+        en: {
+          name: card[3],
+          set_name: card[5] === null ? "Mythical Island" : card[5],
+          image: `https://static.dotgg.gg/pokepocket/card/${card[0]}.webp`,
+          fullName: card[3] + " " + card[0],
+        },
+        fr: {
+          name: null,
+          set_name: null,
+          image: null,
+          fullName: null,
+        },
+        number: parseInt(card[2]),
+      };
+    });
+
+    // Write the cleaned data to a new JSON file
+    await fs.promises.writeFile(
+      "cleaned_test.json",
+      JSON.stringify(cleanedData, null, 2),
+      "utf8"
+    );
+    console.log("File has been cleaned and saved as cleaned_test.json");
+  } catch (error) {
+    console.error("Error fetching the data:", error);
+  }
+}
+
+// // ETAPE 2 : le json fr est fatigue faut jle repare
+
+// JSON FRANCAIS curl -X GET "https://phawpbusbqwdlbxaedev.supabase.co/rest/v1/cards?select=*" -H "apikey: eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InBoYXdwYnVzYnF3ZGxieGFlZGV2Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzQ5NzQ1OTEsImV4cCI6MjA1MDU1MDU5MX0.pY-Zi1AoEuHHNgGB1yPuqy4cyA6vy57W4VSYrar0SiQ" -H "Accept: application/json"
+
+async function ETAPE2() {
+  var extensionFRBySetId = {
+    "puissance-genetique": "A1",
+    "l-ile-fabuleuse": "A1a",
+    "choc-spatio-temporel": "A2",
+    "lumiere-triomphale": "A2a",
+    "rejouissances-rayonnantes": "A2b",
+    "promo-a": "PROMO",
+  };
+
+  // function with a int param, and return the same in string BUT with 0 in front like 1 -> 001, 10 -> 010, 100 -> 100
+  function formatNumber(number) {
+    return number.toString().padStart(3, "0");
+  }
+
+  let data = [];
+  try {
+    const response = await axios.get(
+      "https://phawpbusbqwdlbxaedev.supabase.co/rest/v1/cards?select=*",
+      {
+        headers: {
+          apikey:
+            "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InBoYXdwYnVzYnF3ZGxieGFlZGV2Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzQ5NzQ1OTEsImV4cCI6MjA1MDU1MDU5MX0.pY-Zi1AoEuHHNgGB1yPuqy4cyA6vy57W4VSYrar0SiQ",
+        },
+      }
+    );
+    data = response.data;
+  } catch (error) {
+    console.error("Error fetching the data:", error);
+    throw error;
+  }
+
+  const updatedData = data.map((card) => {
+    const setId = extensionFRBySetId[card.set];
+    return {
+      ...card,
+      pokemonID: setId + "-" + formatNumber(card.card_id),
+    };
+  });
+  // Write the cleaned data to a new JSON file
+  await fs.promises.writeFile(
+    "jsonFR.json",
+    JSON.stringify(updatedData, null, 2),
+    "utf8",
+    (err) => {
+      if (err) {
+        console.error("Error writing the file:", err);
+        return;
+      }
+      console.log("File has been cleaned and saved as jsonFR.json");
+    }
+  );
+}
+
+// ETAPE 3 RAJOUTER LES NOMS FR
+
+async function ETAPE3() {
+  var extensionFRBySetId = {
+    A1: "puissance-genetique",
+    A1a: "l-ile-fabuleuse",
+    A2: "choc-spatio-temporel",
+    A2a: "lumiere-triomphale",
+    A2b: "rejouissances-rayonnantes",
+    "promo-a": "promo-a",
+  };
+
+  const data = JSON.parse(fs.readFileSync("cleaned_test.json", "utf8"));
+  const dataFR = JSON.parse(fs.readFileSync("./jsonFR.json", "utf8"));
+
+  // pour chaque element de data, regarder si il y a le meme id dabs dataFR ET SI OUI, rajouter les fr: {name, set_name, image, fullName}
+
+  var updatedData = data.map((pokemon) => {
+    const matchingCard = dataFR.find((card) => card.pokemonID === pokemon.id);
+    if (matchingCard) {
+      return {
+        ...pokemon,
+        fr: {
+          name: matchingCard.card_fr,
+          set_name: matchingCard.set,
+          image: `https://image.pokemonpocket.fr/${matchingCard.set}/${
+            pokemon.number
+          }.${
+            matchingCard.set === "lumiere-triomphale" ||
+            "rejouissances-rayonnantes"
+              ? "webp"
+              : "png"
+          }`,
+          fullName: matchingCard.card_fr + " " + pokemon.id,
+        },
+      };
+    }
+    return pokemon;
+  });
+
+  await fs.promises.writeFile(
+    "cleaned_test.json",
+    JSON.stringify(updatedData, null, 2),
+    "utf8",
+    (err) => {
+      if (err) {
+        console.error("Error writing the file:", err);
+        return;
+      }
+      console.log("File has been cleaned and saved as cards.json");
+    }
+  );
+}
+
+// QUELLE EXTENSION ET CARTE PEUVENT ETRE TRADE
+async function ETAPE4() {
+  const data1 = JSON.parse(fs.readFileSync("cleaned_test.json", "utf8"));
+  // Mettre à jour les valeurs de set_name_fr et image_fr
+  var updatedData = data1.map((pokemon) => {
+    return {
+      ...pokemon,
+      isTradable:
+        listeExtensions[pokemon.setId]?.isTradable === false
+          ? false
+          : pokemon.rarity < 6,
+    };
+  });
+
+  // Sauvegarder le fichier mis à jour
+  await fs.promises.writeFile(
+    "cleaned_test.json",
+    JSON.stringify(updatedData, null, 2),
+    "utf8"
+  );
+
+  console.log("Transformation terminée !");
+}
+
+// CREER UN FICHIER JSON QUI COMPARE cards.json et cleaned_test.json. Ce json doit avoir 3 objets : nouveau, modifie, supprime.
+// Le fichier cleaned_test.json est la version la plus recente et cards.json est l'ancienne version
+
+async function ETAPE5() {
+  const data1 = JSON.parse(fs.readFileSync("cleaned_test.json", "utf8"));
+  const data2 = JSON.parse(fs.readFileSync("cards.json", "utf8"));
+
+  const newCards = data1.filter(
+    (card1) => !data2.some((card2) => card1.id === card2.id)
+  );
+
+  const modifiedCards = data1
+    .filter((card1) => {
+      const card2 = data2.find((card2) => card1.id === card2.id);
+      return card2 && JSON.stringify(card1) !== JSON.stringify(card2);
+    })
+    .map((card1) => {
+      const card2 = data2.find((card2) => card1.id === card2.id);
+      const changes = {};
+      for (const key in card1) {
+        if (JSON.stringify(card1[key]) !== JSON.stringify(card2[key])) {
+          changes[key] = { old: card2[key], new: card1[key] };
+        }
+      }
+      return { id: card1.id, changes };
+    });
+
+  const deletedCards = data2.filter(
+    (card2) => !data1.some((card1) => card1.id === card2.id)
+  );
+
+  const result = {
+    new: newCards,
+    modified: modifiedCards,
+    deleted: deletedCards,
+  };
+
+  await fs.promises.writeFile(
+    "compare.json",
+    JSON.stringify(result, null, 2),
+    "utf8"
+  );
+}
+
+// (async function main() {
+//   await ETAPE1();
+//   await ETAPE2();
+//   await ETAPE3();
+//   await ETAPE4();
+//   await ETAPE5();
+// })();
+
+// deleteFiles();
+
+// GENERE LA LISTE DES EXTENSIONS UNIQUES
+// const dataEN = JSON.parse(fs.readFileSync('./cleaned_test.json', 'utf8'));
+
+// // Utiliser un objet pour filtrer les extensions uniques
+// const extensionsMap = {};
+
+// dataEN.forEach(item => {
+//     if (!extensionsMap[item.setId]) {
+//         extensionsMap[item.setId] = {
+//             extension_id: item.setId,
+//             extension_en: item.en.set_name,
+//             extension_fr: item.fr.set_name,
+//             isTradable: null
+//         };
+//     }
+// });
+
+// // Écrire dans un fichier JSON
+// fs.writeFileSync('listeExtensions.json', JSON.stringify(extensionsMap , null, 2), 'utf8', (err) => {
 //     if (err) {
-//         console.error('Error reading the file:', err);
+//         console.error('Error writing the file:', err);
 //         return;
 //     }
-
-//     // Parse the JSON data
-//     const jsonData = JSON.parse(data);
-
-//     // Extract the relevant fields
-//     const cleanedData = jsonData.map(card => {
-//         // Vérifie si card.props est défini et est un tableau
-//         const dupeReward = Array.isArray(card.props)
-//             ? card.props.find(prop => prop.name === "Dupe Reward")?.value ?? null
-//             : null;
-    
-//         const packPoint = Array.isArray(card.props)
-//             ? card.props.find(prop => prop.name === "Pack Point")?.value ?? null
-//             : null;
-    
-//         // Détermine la rareté en fonction de packPoint
-//         const rarityMap = {
-//             35: 1,
-//             70: 2,
-//             150: 3,
-//             500: 4,
-//             400: 5,
-//             1250: 6,
-//             1500: 7,
-//             2500: 8
-//         };
-        
-//         const rarity = rarityMap[packPoint] ?? null; // Définit null si packPoint n'est pas dans la liste
-    
-//         // Détermine le coût d'échange en fonction de la rareté
-//         const tradeCostMap = {
-//             1: 0,
-//             2: 0,
-//             3: 120,
-//             4: 500,
-//             5: 400
-//         };
-    
-//         const tradeCost = tradeCostMap[rarity] ?? null; // Définit null si la rareté n'est pas dans la liste
-    
-//         return {
-//             id: card.id,
-//             setId: card.setId,
-//             number: card.number,
-//             name: card.name,
-//             set_code: card.set_code,
-//             set_name: card.set_name,
-//             slug: card.slug,
-//             dupe_reward: dupeReward,
-//             pack_point: packPoint,
-//             rarity: rarity,
-//             trade_cost: tradeCost,
-//             image: `${card.id}.webp`,
-//         };
-//     });
-    
-    
-
-//     // Write the cleaned data to a new JSON file
-//     fs.writeFile('cleaned_test.json', JSON.stringify(cleanedData, null, 2), 'utf8', err => {
-//         if (err) {
-//             console.error('Error writing the file:', err);
-//             return;
-//         }
-//         console.log('File has been cleaned and saved as cleaned_test.json');
-//     });
+//     console.log('File has been cleaned and saved as listeExtensions.json');
 // });
-
-
-// Charger les fichiers JSON TRADUIT LE NOM DES POKEMONS
-// const data1 = JSON.parse(fs.readFileSync('cards.json', 'utf8'));
-// const translations = JSON.parse(fs.readFileSync('traduction.json', 'utf8'));
-
-// // Mettre à jour chaque élément
-// const updatedData = data1.map(pokemon => {
-//     return {
-//         ...pokemon,
-//         name_fr: translations[pokemon.name] || pokemon.name // Si pas de traduction, garder le nom original
-//     };
-// });
-
-// // Sauvegarder le fichier mis à jour
-// fs.writeFileSync('updated_data.json', JSON.stringify(updatedData, null, 2), 'utf8');
-
-// console.log('Mise à jour terminée !');
-
-
-
-// // Charger les fichiers JSON TRADUIT LE NOM DES POKEMONS
-// const data1 = JSON.parse(fs.readFileSync('cards.json', 'utf8'));
-// // const translations = JSON.parse(fs.readFileSync('traduction.json', 'utf8'));
-
-// // Mettre à jour chaque élément
-// const updatedData = data1.map(pokemon => {
-//     return {
-//         ...pokemon,
-//         // parse string to int dupe_reward and pack_point
-//         dupe_reward: parseInt(pokemon.dupe_reward),
-//         pack_point: parseInt(pokemon.pack_point),
-//     };
-// });
-
-// // Sauvegarder le fichier mis à jour
-// fs.writeFileSync('fr.json', JSON.stringify(updatedData, null, 2), 'utf8');
-
-// console.log('Mise à jour terminée !');
-
-// const data1 = JSON.parse(fs.readFileSync('cards.json', 'utf8'));
-// // Mettre à jour les valeurs de set_name_fr et image_fr
-
-// const updatedData = data1.map(pokemon => {
-//     return {
-//         ...pokemon,
-//         en: {
-//             ...pokemon.en,
-//              "set_name": pokemon.setId === "PROMO" ? "Promo-A" : pokemon.en.set_name,
-//         },
-//         fr: {
-//             ...pokemon.fr,
-//             "set_name": pokemon.setId === "PROMO" ? "promo-a" : pokemon.en.set_name,
-//             "image": pokemon.setId === "PROMO" ? `https://image.pokemonpocket.fr/promo-a/${pokemon.number}.png` : pokemon.fr.image
-//         }
-//     };
-// });
-
-// // Sauvegarder le fichier mis à jour
-// fs.writeFileSync('cards.json', JSON.stringify(updatedData, null, 2), 'utf8');
-
-// console.log('Transformation terminée !');
-
-const data1 = JSON.parse(fs.readFileSync('cards.json', 'utf8'));
-
-const updatedData = data1.map(pokemon => {
-    return {
-        ...pokemon,
-        en: {
-            ...pokemon.en,
-            "image": `https://static.dotgg.gg/pokepocket/card/${pokemon.id}.webp`
-        },
-    };
-}
-);
-
-// Sauvegarder le fichier mis à jour
-fs.writeFileSync('cards.json', JSON.stringify(updatedData, null, 2), 'utf8');
-
-
-
-// // Charger les fichiers JSON
-// const frData = JSON.parse(fs.readFileSync('fr.json', 'utf8'));
-// const cardsData = JSON.parse(fs.readFileSync('cards.json', 'utf8'));
-
-// // Créer un dictionnaire pour un accès rapide aux traductions
-// const translationMap = {};
-// frData.forEach(entry => {
-//     translationMap[entry.card_en] = entry.card_fr;
-// });
-
-// // Mettre à jour les noms français dans cards.json
-// cardsData.forEach(card => {
-//     if (translationMap[card.name_en]) {
-//         card.name_fr = translationMap[card.name_en];
-//     }
-// });
-
-// // Sauvegarder les modifications dans cards.json
-// fs.writeFileSync('cards_test.json', JSON.stringify(cardsData, null, 2), 'utf8');
-
-// console.log('Mise à jour terminée !');
